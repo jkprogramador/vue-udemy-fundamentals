ce30eded5db50c9f529a36e5f8d9715009e51aa2
Jack Lee <br.sp.jkemail@gmail.com>
2022-03-31 09:59:15 -0300

    Outputting raw HTML

--- js/app.js
+++ js/app.js
@@ -3,7 +3,8 @@ const vm = Vue.createApp({
         return {
             firstName: 'John',
             lastName: 'Doe',
-            url: 'https://google.com'
+            url: 'https://google.com',
+            rawUrl: '<a href="https://google.com">Google</a>'
         }
     },
     methods: {
