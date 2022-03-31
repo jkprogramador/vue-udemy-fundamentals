1304266019e63ff30c3e1a86b70144d8a7730d0c
Jack Lee <br.sp.jkemail@gmail.com>
2022-03-31 10:06:51 -0300

    Listening to events

--- js/app.js
+++ js/app.js
@@ -4,12 +4,19 @@ const vm = Vue.createApp({
             firstName: 'John',
             lastName: 'Doe',
             url: 'https://google.com',
-            rawUrl: '<a href="https://google.com">Google</a>'
+            rawUrl: '<a href="https://google.com">Google</a>',
+            age: 20
         }
     },
     methods: {
         fullName() {
             return `${this.firstName} ${this.lastName.toUpperCase()}`
+        },
+        increment() {
+            this.age++
+        },
+        updateLastName(event) {
+            this.lastName = event.target.value
         }
     }
 }).mount('#app');
