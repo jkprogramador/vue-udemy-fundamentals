bcd9a57a155f969d7a771cc306285b522c8e6844
Jack Lee <br.sp.jkemail@gmail.com>
2022-03-31 13:40:20 -0300

    List rendering

--- js/app.js
+++ js/app.js
@@ -1,7 +1,12 @@
 const vm = Vue.createApp({
     data() {
         return {
-            mode: 1
+            birds: ['Pigeons', 'Eagles', 'Doves', 'Parrots'],
+            people: [
+                { name: 'John', age: 20 },
+                { name: 'Rick', age: 18 },
+                { name: 'Amy', age: 33 },
+            ]
         }
     }
 }).mount('#app');
