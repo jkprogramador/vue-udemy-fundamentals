27753ddbb309a78eac59517a9fc1c1c9f570cae3
Jack Lee <br.sp.jkemail@gmail.com>
2022-03-31 13:18:23 -0300

    Conditional rendering

--- js/app.js
+++ js/app.js
@@ -1,17 +1,7 @@
 const vm = Vue.createApp({
     data() {
         return {
-            isPurple: false,
-            selectedColor: '',
-            size: 150
-        }
-    },
-    computed: {
-        circleClasses() {
-            return {
-                // object syntax where key is class name and value is the condition to check to determine whether to include class
-                purple: this.isPurple
-            }
+            mode: 1
         }
     }
 }).mount('#app');
