3dfa526fb955c274b96d044c24c936b4340d73c0
Jack Lee <br.sp.jkemail@gmail.com>
2022-03-31 10:20:54 -0300

    Keyboard events and modifiers

--- js/app.js
+++ js/app.js
@@ -3,6 +3,7 @@ const vm = Vue.createApp({
         return {
             firstName: 'John',
             lastName: 'Doe',
+            middleName: '',
             url: 'https://google.com',
             rawUrl: '<a href="https://google.com">Google</a>',
             age: 20
@@ -10,7 +11,7 @@ const vm = Vue.createApp({
     },
     methods: {
         fullName() {
-            return `${this.firstName} ${this.lastName.toUpperCase()}`
+            return `${this.firstName} ${this.middleName} ${this.lastName.toUpperCase()}`
         },
         increment() {
             this.age++
@@ -18,6 +19,9 @@ const vm = Vue.createApp({
         updateLastName(msg, event) {
             console.log(msg)
             this.lastName = event.target.value
+        },
+        updateMiddleName(event) {
+            this.middleName = event.target.value
         }
     }
 }).mount('#app');
